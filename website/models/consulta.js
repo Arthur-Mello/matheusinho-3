d43507879908a01144018c844c2b28553c2ce7dc
Matheus Garvão <100000464+MatheusGarvao@users.noreply.github.com>
2023-12-01 19:01:30 -0300

    WebApp done

--- website/models/consulta.js
+++ website/models/consulta.js
@@ -1,46 +1,45 @@
-'use strict';
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class Consulta extends Model {
-    static associate(models) {
-      Consulta.belongsTo(models.User, { foreignKey: 'id_paciente', as: 'paciente' });
-      Consulta.belongsTo(models.User, { foreignKey: 'id_medico', as: 'medico' });
-    }
-  }
-  Consulta.init(
-    {
-      id_paciente: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      id_medico: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      data: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      horarioAgendado: {
-        type: DataTypes.TIME,
-        allowNull: false,
-      },
-      prontuario: {
-        type: DataTypes.STRING,
-      },
-      receita: {
-        type: DataTypes.STRING,
-      },
-      atestado: {
-        type: DataTypes.STRING,
-      },
+  const Consulta = sequelize.define('Consulta', {
+    id_paciente: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-    {
-      sequelize,
-      modelName: 'Consulta',
-      tableName: 'Consultas',
-    }
-  );
+    id_medico: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dataAgendada: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    prontuario: {
+      type: DataTypes.STRING,
+    },
+    receita: {
+      type: DataTypes.STRING,
+    },
+    atestado: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    sequelize,
+    modelName: 'Consulta',
+    tableName: 'Consultas',
+  });
+
+  Consulta.associate = (models) => {
+    
+    Consulta.belongsTo(models.User, {
+      as: 'paciente',
+      foreignKey: 'id_paciente',
+    });
+    
+    Consulta.belongsTo(models.User, {
+      as: 'medico',
+      foreignKey: 'id_medico',
+    });
+    
+  };
+
   return Consulta;
 };
